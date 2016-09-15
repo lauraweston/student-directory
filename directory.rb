cd6fd6a152a3d3fd3a7f245ba5ba43371f2096ca
Laura Weston <laura.weston@outlook.com>
2016-09-15 12:35:59 +0100

    Refactor search students action

--- directory.rb
+++ directory.rb
@@ -12,11 +12,7 @@ def interactive_menu
     when "2"
       show_students(student_list)
     when "3"
-      if !student_list.empty?
-          search(student_list)
-      else
-          puts "We have no current students"
-      end
+      search(student_list)
     end
   end
   puts "Goodbye!"
@@ -38,8 +34,7 @@ end
 def get_user_input(prompt, default="")
   print prompt + ": "
   input = gets.strip
-  return default if input.empty?
-  input
+  input.empty? ? default : input
 end
 
 def get_current_month
@@ -50,9 +45,8 @@ def input_students
   puts "To return to the menu, just hit return twice\n"
   students = []
   name = get_user_input("Please enter the name of a student").capitalize
-  cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
-
-  while !name.empty? do
+  while !name.empty?
+    cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
     cohort = nil
     until cohorts.include?(cohort)
       cohort = get_user_input("Cohort", get_current_month).downcase.to_sym
@@ -62,7 +56,6 @@ def input_students
     hobbies = get_user_input("Hobbies", "Unknown")
 
     students << { name: name, cohort: cohort, age: age, nationality: nationality, hobbies: hobbies }
-
     puts "#{students.count} new #{pluralize(students.count, "student")} enrolled"
     name = get_user_input("Please enter the name of a student").capitalize
   end
@@ -120,16 +113,18 @@ def show_students(students, message=nil)
 end
 
 def search(students)
-  puts "To search for students, enter the first letter or first few letters of the name and press Enter."
-  search_term = gets.strip.downcase
-  filtered_student_list = students.select do |student|
-    student[:name].downcase.start_with?(search_term)
-  end
-  if filtered_student_list.empty?
-    puts "No matching students found"
+  if students.empty?
+    puts "We have no current students"
   else
-    message = "#{filtered_student_list.count} #{pluralize(filtered_student_list.count, "student")} found"
-    show_students(filtered_student_list, message)
+    puts "To search for students, enter the first letter or first few letters of the name and press Enter."
+    search_term = gets.strip.downcase
+    filtered_students = students.select { |student| student[:name].downcase.start_with?(search_term) }
+    if filtered_students.empty?
+      puts "No matching students found"
+    else
+      message = "#{filtered_students.count} #{pluralize(filtered_students.count, "student")} found"
+      show_students(filtered_students, message)
+    end
   end
 end
 
